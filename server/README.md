822f4b17a0b22711ee2837c8c223636aa90fd5d9
Iain J McCallum <ijmccallum@hotmail.co.uk>
2015-05-26 20:40:16 -0400

    Clear out

--- server/README.md
+++
@@ -1,7 +0,0 @@
-#The WikiLogic Express.js Server
-
----
-
-List of things to whip up:
-
-1. figure this list out
--- server/router/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * The Index of Routes
- */
-
-module.exports = function (app) {
-
-    // The signup route
-    app.use('/signup', require('./routes/signup'));
-}
