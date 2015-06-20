8ff152a51c4b64ea1d74bf048a913db35cb30665
Iain J McCallum <ijmccallum@hotmail.co.uk>
2015-06-20 19:43:52 -0400

    issue #54: publish button hooked up to controller function

--- public/editor/controllers/myDrafts-controller.js
+++ public/editor/controllers/myDrafts-controller.js
@@ -1,56 +1,16 @@
 'use strict';
 /**
- * The Editor list controller,
- * deals with the various types of lists that get laied out
- * the controller name is a duplicate of the Explorer as the list controlls are common but function differently in each app
+ * 
  */
 
 Editor.controller('MyDraftsController', ['$scope', '$rootScope', 'getterOfUsers', function($scope, $rootScope, getterOfUsers) {
 
  	 /*
- 	  * Once the client side is loaded - this fires off another request to get the list
+ 	  * 
  	  */
- 	 $scope.setUserData = function(){
- 	 	//0. Don't need the user's id - it's set in the session!
-
- 	 	//1. ask getterOfUsers service for this users data
- 	 	/*
- 	 	getterOfUsers.getMyData().success(function(result){
-			console.log('got user data!', result);
-		}).error(function(){
-			console.log('getting the user data failed somehow');
-		});
-		*/
-
-		//3. Inject this users data into the DOM
-		/*
- 	 	var draftClaimsArray = [];
- 	 	publishedClaimsArry = [];
- 	 	$rootScope.list = {
- 	 		draftClaims : draftClaimsArray,
- 	 		publishedClaims : publishedClaimsArry
- 	 	}
- 	 	*/
+ 	 $scope.publishClaim = function(claim){
+ 	 	console.log('going to publish ', claim);
  	 }
 
-
- 	 /*
- 	  * Going to have to refactor this
- 	  */
- 	 $scope.setListOrderTo = function(sortBy){
-
-		getterOfClaims.getListOfClaimsBy(sortBy).success(function(result){
-
-			var listArray = result;
-			
-			$rootScope.list.claims = listArray;
-			$rootscope.list.listType = 'recent';
-			
-
-		}).error(function(){
-			console.log('getting the claims failed somehow');
-		});
-		
- 	 }
 
 }]);
