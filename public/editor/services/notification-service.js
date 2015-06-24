de1bb5fb102b989b2ee35beaed57c64a350236a9
Iain J McCallum <ijmccallum@hotmail.co.uk>
2015-06-24 07:19:26 -0400

    issue #80: notification service stubbed

---
+++ public/editor/services/notification-service.js
@@ -0,0 +1,23 @@
+'use strict';
+/*
+ * These services handle the saving of claims, they're set as an editor service 
+ * as it's a restricted activity (not on the client side - on the server side)
+ */
+
+angular.module('Editor')
+.factory('notifyUser',['$http',
+	function($http){
+		var service = {
+			goodThing: function(message){
+				console.log('good thing: ', message);
+			},
+			badThing: function(message){
+				console.log('bad thing: ', message);
+			},
+			normalThing: function(message) {
+				console.log('normal thing: ', message);
+			}
+		};
+		return service;
+	}]
+);
