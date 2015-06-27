980f5a418eec59ca49aeec01c8e00ab41bae07f1
Iain J McCallum <ijmccallum@hotmail.co.uk>
2015-06-27 18:14:57 -0400

    set up routing, not working properly yet though

--- public/editor/editor-app.js
+++ public/editor/editor-app.js
@@ -1,3 +1,18 @@
 'use strict';
 //The editor requires the explorer as it builds onto it.
-var Editor = angular.module('Editor', ['Explorer']);
+var Editor = angular.module('Editor', ['Explorer', 'ngRoute']);
+	
+Editor.config(function($routeProvider){
+	console.log('setting up the routes');
+	$routeProvider
+		.when("/editor", {
+			templateUrl: "/partials/newClaim-partial.html",
+			controller: "NewDraftController"
+		})
+		.when("/editor/my-drafts",	{
+			templateUrl: "/partials/drafts/myDraftsList-partial.html",
+			controller: "MyDraftsController"
+		});
+	//End routeProvider
+
+});
