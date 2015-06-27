8f8ac264afd1e4fcb3afd2472df7fc27fab149d7
Iain J McCallum <ijmccallum@hotmail.co.uk>
2015-06-27 17:22:08 -0400

    collapsable toggle, not too happy with it though

--- public/js/wikiLogicUIhelpers.js
+++ public/js/wikiLogicUIhelpers.js
@@ -1,11 +1,22 @@
 var wikiLogicUI = {
     'init': function()
     {
-    	console.log('loaded!');
         this.initExpander();
     },
 	'initExpander' : function(){
-		//find .js-expander, register click on it's button
+		/*
+		 * Gives any element with the class 'js-collapsable' a button to collaps the next sibling
+		 */
+		$('.js-collapsable-title').each(function(){
+			//$(this).prepend('<img src="/icons/black/plus.svg" alt="" class="js-collapsable--btn">');
+			$(this).prepend('<div class="js-collapsable--btn"></div>');
+			$(this).find('.js-collapsable--btn').on('click', function(){
+				console.log('collapse btn clicked!', $(this).parent());
+				//animate slide shut
+				$(this).parent().next('.js-collapsable-content').slideToggle();
+				$(this).parent().toggleClass('collapsed');
+			});
+		});
 	}
 };
 
@@ -13,4 +24,4 @@ var wikiLogicUI = {
 /* window load might flicker, maybe use jQuery(document).ready(function($) { */
 $(window).load(function(){
 	 wikiLogicUI.init();
-});
+});	
