cfad89b514489d5095a7b1b021f2656621e2905b
Iain J McCallum <ijmccallum@hotmail.co.uk>
2015-07-06 21:16:25 -0400

    draft claim input styled

--- public/editor/draftEditor/draftEditor-controller.js
+++ public/editor/draftEditor/draftEditor-controller.js
@@ -18,6 +18,14 @@ Editor.controller('draftEditorController', ['$scope', '$rootScope', '$routeParam
 				}
 			});
 		}
+
+		$('textarea').keypress(function(event) {
+			// Check the keyCode and if the user pressed Enter (code = 13) 
+			// disable it
+			if (event.keyCode == 13) {
+				event.preventDefault();
+			}
+		});
 	}
 
 	var setCurrentDraft = function(draftID){
