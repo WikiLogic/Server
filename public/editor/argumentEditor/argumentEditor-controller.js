924395dcc918a7d5e1e1b56e53ff9b3b17725b8d
Iain J McCallum <ijmccallum@hotmail.co.uk>
2015-07-10 17:32:47 -0400

    #109: search result select now replacing reason

--- public/editor/argumentEditor/argumentEditor-controller.js
+++ public/editor/argumentEditor/argumentEditor-controller.js
@@ -1,19 +1,17 @@
 'use strict';
-/**
- * This gets called twice - once for each side of the arguments.
- * the partial calls init() and passes the side it's on (for / against)
- *
- */
+
 
 Editor.controller('argumentListController', ['$scope', '$rootScope', 'saviorOfClaims', function($scope, $rootScope, saviorOfClaims) {
 
  	
 	/*
-	 * this controller gets called twice - once for each side: supporting / opposing
+	 * this controller gets called for as many args as there are and each passes the side it's on - once for each side: supporting / opposing
 	 * The init sets the side so we know which side we're on!
 	 */
-	$scope.init = function(side) {
+	$scope.init = function(side, argIndex) {
+		console.log('arg index set: ', argIndex);
 		$scope.side = side;
+		$scope.argIndex = argIndex;
 	}
 
 	$scope.addReason = function(argIndex){
@@ -26,8 +24,17 @@ Editor.controller('argumentListController', ['$scope', '$rootScope', 'saviorOfCl
 
 	$scope.changeReason = function(reasonIndex, reasonDecription) {
 		//maybe wait a bit?
-		console.log('changing reason', reasonDecription, ' from arg ', reasonIndex ,' on ', $scope.side);
+		console.log('changing reason', reasonDecription, ' from reason num ', reasonIndex ,' in arg num ', $scope.argIndex , ' on ', $scope.side);
 		$rootScope.claimSearch = reasonDecription;
+
+		//now watch for a selection from the search
+		$rootScope.$watch('searchSelection', function(newVal,oldVal){
+			if (newVal === oldVal) {
+				//called to init
+			} else {
+				$rootScope.currentDraft[$scope.side][$scope.argIndex].reasons[reasonIndex] = newVal;
+			}
+		});
 		//$rootScope.claimSearch = $rootScope.currentDraft.description;
 		//set which reason is in focus
 	}
