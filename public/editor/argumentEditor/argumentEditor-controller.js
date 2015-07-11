4324e47e6ec7e1766fe894420d5f45843380b169
Iain J McCallum <ijmccallum@hotmail.co.uk>
2015-07-10 20:40:46 -0400

    #115: created!

--- public/editor/argumentEditor/argumentEditor-controller.js
+++ public/editor/argumentEditor/argumentEditor-controller.js
@@ -5,8 +5,8 @@ Editor.controller('argumentListController', ['$scope', '$rootScope', 'saviorOfCl
 
  	
 	/*
-	 * this controller gets called for as many args as there are and each passes the side it's on - once for each side: supporting / opposing
-	 * The init sets the side so we know which side we're on!
+	 * Each argument has it's own instance of this controller, 
+	 * the init stores a refrece to the argument index and which side it's on.
 	 */
 	$scope.init = function(side, argIndex) {
 		console.log('arg index set: ', argIndex);
@@ -24,15 +24,20 @@ Editor.controller('argumentListController', ['$scope', '$rootScope', 'saviorOfCl
 
 	$scope.changeReason = function(reasonIndex, reasonDecription) {
 		//maybe wait a bit?
-		console.log('changing reason', reasonDecription, ' from reason num ', reasonIndex ,' in arg num ', $scope.argIndex , ' on ', $scope.side);
 		$rootScope.claimSearch = reasonDecription;
 
-		//now watch for a selection from the search
-		$rootScope.$watch('searchSelection', function(newVal,oldVal){
+		//set this reason to active, change state.  -- need to figure out how to turn off active state
+
+		//now watch for a selection from the search (which is global)
+		//Maybe create a search service - which returns when selected
+		var listener = $rootScope.$watch('searchSelection', function(newVal,oldVal){
 			if (newVal === oldVal) {
 				//called to init
 			} else {
+				//set the selected search item and assign it to our reason
 				$rootScope.currentDraft[$scope.side][$scope.argIndex].reasons[reasonIndex] = newVal;
+				listener(); //clear the watch?
+
 			}
 		});
 		//$rootScope.claimSearch = $rootScope.currentDraft.description;
