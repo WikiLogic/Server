3fe5f705729ffa8caf9d69e558005f8185861861
Iain J McCallum <ijmccallum@hotmail.co.uk>
2016-05-28 17:04:20 -0400

    dynamic port setting for heroku

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ console.timeEnd('     1: Setting up the routes');
 ==========================================*/
 console.timeEnd('    0: Everything ready!');
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
